f52f8c9b302d08ff775e0c5f38f108e1b48eb858
V <victorluquebuxade@gmail.com>
2023-04-16 19:50:23 +0200

    add example search URL from linkedin to scrap jobs

--- cheerioCrawler/src/main.js
+++ cheerioCrawler/src/main.js
@@ -2,7 +2,7 @@
 import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
 
-const startUrls = ['https://crawlee.dev'];
+const startUrls = ['https://www.linkedin.com/jobs/search/?currentJobId=3329814289&geoId=107025191&keywords=node&location=Barcelona%2C%20Catalu%C3%B1a%2C%20Espa%C3%B1a&refresh=true'];
 
 const crawler = new CheerioCrawler({
     // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
