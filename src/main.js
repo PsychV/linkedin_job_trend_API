894276cacbf79c28b1dee747357a6022f30445de
V <victorluquebuxade@gmail.com>
2023-04-19 17:00:45 +0200

    remove folder cheerioScraper and put its content in root directory

--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import { router } from './routes.js';
 const startUrls = ['https://www.linkedin.com/jobs/search/?currentJobId=3329814289&geoId=107025191&keywords=node&location=Barcelona%2C%20Catalu%C3%B1a%2C%20Espa%C3%B1a&refresh=true'];
 
 const crawler = new CheerioCrawler({
-    // proxyConfiguration: new ProxyConfiguration({ proxyUrls: ['...'] }),
+    maxConcurrency: 1,
+    maxRequestsPerCrawl: 1000, 
+    maxRequestsPerMinute: 1, // Linkedin limit seems to be around 900 requests per hour -> https://github.com/tomquirk/linkedin-api
     requestHandler: router,
 });
 
