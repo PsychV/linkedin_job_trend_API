1551ad261c017311f09946684fcb46a452a272d7
V <victorluquebuxade@gmail.com>
2023-04-23 15:56:28 +0200

    add proxyPool for webscraping

--- src/main.js
+++ src/main.js
@@ -1,13 +1,12 @@
 // For more information, see https://crawlee.dev/
 import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
+import proxyPool from './proxyPool.js';
 
 const startUrls = ['https://www.linkedin.com/jobs/search/?currentJobId=3329814289&geoId=107025191&keywords=node&location=Barcelona%2C%20Catalu%C3%B1a%2C%20Espa%C3%B1a&refresh=true'];
 
 const proxyConfiguration = new ProxyConfiguration({
-    proxyUrls: [
-        "https://proxy-example.com",
-    ],
+    proxyUrls: proxyPool,
 });
 
 const crawler = new CheerioCrawler({
