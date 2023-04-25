0b7edcd246c8d11b531d6b7cc0f84bfe8a9fc53d
V <victorluquebuxade@gmail.com>
2023-04-25 20:34:52 +0200

    get job description from scraped jobs

--- src/routes.js
+++ src/routes.js
@@ -11,12 +11,18 @@ router.addDefaultHandler(async ({ enqueueLinks, log }) => {
 });
 
 router.addHandler('detail', async ({ request, $, log }) => {
+    
     await sleep(500 + (Math.random() * 1500));
+
     const title = $('title').text();
     log.info(`${title}`, { url: request.loadedUrl });
+
+    const jobDescription = $('.core-section-container__content').text().replace(/\s{2,}/g," ");
+    log.info(`${jobDescription.substring(0, 300)}`);
     
     await Dataset.pushData({
         url: request.loadedUrl,
         title,
+        jobDescription,
     });
 });
