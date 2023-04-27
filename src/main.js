347c2a8cbb948623b327f9798d3ad0ec8258321b
V <victorluquebuxade@gmail.com>
2023-04-27 20:33:18 +0200

    change search job url to avoid scrolling down page

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@ import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
 import proxyPool from './proxyPool.js';
 
-const startUrls = ['https://www.linkedin.com/jobs/search/?currentJobId=3329814289&geoId=107025191&keywords=node&location=Barcelona%2C%20Catalu%C3%B1a%2C%20Espa%C3%B1a&refresh=true'];
+const startUrls = ['https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?currentJobId=3560813328&distance=25&geoId=107025191&keywords=node&location=Barcelona, Cataluña, España&refresh=true&start=25'];
+// 25 * 39 = los casi 975 jobs que muestran publicos
 
 const proxyConfiguration = new ProxyConfiguration({
     proxyUrls: proxyPool,
@@ -15,6 +16,7 @@ const crawler = new CheerioCrawler({
     maxRequestsPerCrawl: 1000, 
     maxRequestsPerMinute: 15, // Linkedin limit seems to be around 900 requests per hour -> https://github.com/tomquirk/linkedin-api
     requestHandler: router,
+    
 });
 
 await crawler.run(startUrls);
