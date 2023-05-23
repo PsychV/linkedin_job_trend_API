1e63209b03363702e2126972504eb54fa1e9ef54
V <victorluquebuxade@gmail.com>
2023-05-23 15:51:03 +0200

    eliminate proxy configuration

--- src/main.js
+++ src/main.js
@@ -3,15 +3,10 @@ import { CheerioCrawler, ProxyConfiguration } from 'crawlee';
 import { router } from './routes.js';
 import proxyPool from './proxyPool.js';
 
-const startUrls = ['https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?currentJobId=3560813328&distance=25&geoId=107025191&keywords=node&location=Barcelona, Cataluña, España&refresh=true&start=25'];
+const startUrls = ['https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords=node&location=Barcelona, Cataluña, España&refresh=true&start=25'];
 // 25 * 39 = los casi 975 jobs que muestran publicos
 
-const proxyConfiguration = new ProxyConfiguration({
-    proxyUrls: proxyPool,
-});
-
 const crawler = new CheerioCrawler({
-    proxyConfiguration,
     maxConcurrency: 1,
     maxRequestsPerCrawl: 1000, 
     maxRequestsPerMinute: 15, // Linkedin limit seems to be around 900 requests per hour -> https://github.com/tomquirk/linkedin-api
